29ec2a09a4494bc199e1efc45ea8f134f8c50123
David Nabergoj <dn9134@student.uni-lj.si>
2022-06-29 13:27:11 +0200

    Add boilerplate for Flow tests

---
+++ tests/flow.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class FlowTestCase(unittest.TestCase):
+    def test_forward(self):
+        # Test that the forward pass works without raising an error
+        pass
+
+    def test_inverse(self):
+        # Test that the inverse pass works without raising an error
+        pass
+
+    def test_logprob(self):
+        # Test that logprob works without raising an error
+        pass
+
+    def test_sample(self):
+        # Test that sample works without raising an error
+        pass
+
+    def test_reconstruction(self):
+        # Test that latent points are reconstructed to be close enough to data points
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
