Update tests, fix nan batch variance when n=1

pocomc/maf.py
@@ -91,7 +91,10 @@ class BatchNorm(nn.Module):
     def forward(self, x, cond_y=None):
         if self.training:
             self.batch_mean = x.mean(0)
-            self.batch_var = x.var(0)  # note MAF paper uses biased variance estimate; ie x.var(0, unbiased=False)
+
+            # note MAF paper uses biased variance estimate; ie x.var(0, unbiased=False)
+            # If x has a single example, we set the variance to 1.
+            self.batch_var = x.var(0) if len(x) > 1 else torch.ones_like(self.batch_mean)
 
             # update running mean
             self.running_mean.mul_(self.momentum).add_(self.batch_mean.data * (1 - self.momentum))

pocomc/tools.py
@@ -152,7 +152,7 @@ def numpy_to_torch(x):
 
 def torch_double_to_float(x, warn=True):
     if x.dtype == torch.float64 and warn:
-        warnings.warn(f"Float64 data is currently unsupported, casting to Float32.")
+        warnings.warn(f"Float64 data is currently unsupported, casting to Float32. Output will also have type Float32.")
         return x.float()
     elif x.dtype == torch.float32:
         return x