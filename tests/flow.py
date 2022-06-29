8b0921538a47330f7bfb7df07b6be834d20dab51
David Nabergoj <dn9134@student.uni-lj.si>
2022-06-29 13:34:41 +0200

    Update Flow tests boilerplate

--- tests/flow.py
+++ tests/flow.py
@@ -1,7 +1,24 @@
 import unittest
+import torch
 
 
 class FlowTestCase(unittest.TestCase):
+    @staticmethod
+    def make_data():
+        # Make a dataset to use in tests
+        n_data = 100
+        n_dim = 10
+        x = torch.randn(size=(n_data, n_dim))
+        return x
+
+    @staticmethod
+    def make_flow():
+        # Make a Flow to use in tests
+        n_data = 100
+        n_dim = 10
+        x = torch.randn(size=(n_data, n_dim))
+        return x
+
     def test_forward(self):
         # Test that the forward pass works without raising an error
         pass
@@ -20,6 +37,18 @@ class FlowTestCase(unittest.TestCase):
 
     def test_reconstruction(self):
         # Test that latent points are reconstructed to be close enough to data points
+        pass
+
+    def test_logprob_float32(self):
+        # Test logprob when input is torch.float
+        pass
+
+    def test_logprob_float64(self):
+        # Test logprob when input is torch.double
+        pass
+
+    def test_logprob_float(self):
+        # Test logprob when input is float (basic python datatype)
         pass
 
 
