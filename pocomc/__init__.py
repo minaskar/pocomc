5336fc3df6acb4391d49723b8393b8b4e93669ed
minaskar <minaskar@gmail.com>
2022-07-02 16:30:29 +0100

    added info in _init_

--- pocomc/__init__.py
+++ pocomc/__init__.py
@@ -1,3 +1,18 @@
+__bibtex__ = """
+@article{karamanis2022pocomc,
+  title={Accelerating astronomical and cosmological inference with Preconditioned Monte Carlo},
+  author={Karamanis, Minas and Beutler, Florian and Peacock, John A and Nabergoj, David, and Seljak, Uro\v{s}},
+  journal={in prep},
+  year={2022}
+}
+"""
+__url__ = "https://pocomc.readthedocs.io"
+__author__ = "Minas Karamanis"
+__email__ = "minaskar@gmail.com"
+__license__ = "GPL-3.0"
+__description__ = "A Python implementation of Preconditioned Monte Carlo for accelerated Bayesian Computation"
+
+
 from .flow import *
 from .sampler import *
 from .plotting import *
