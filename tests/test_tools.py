c9b48976895dae541fef7c4054d44d3a9955ad6f
minaskar <minaskar@gmail.com>
2022-07-09 17:03:06 +0100

    fixed typo

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -7,11 +7,11 @@ from pocomc.tools import compute_ess
 
 class ESSTestCase(unittest.TestCase):
     def test_ess_single_particle(self):
-        self.assertEqual(compute_ESS(np.array([1.0])), 1.0)
-        self.assertEqual(compute_ESS(np.array([251.0])), 1.0)
-        self.assertEqual(compute_ESS(np.array([-421.0])), 1.0)
-        self.assertEqual(compute_ESS(np.array([-421.125251])), 1.0)
-        self.assertEqual(compute_ESS(np.array([0.0])), 1.0)
+        self.assertEqual(compute_ess(np.array([1.0])), 1.0)
+        self.assertEqual(compute_ess(np.array([251.0])), 1.0)
+        self.assertEqual(compute_ess(np.array([-421.0])), 1.0)
+        self.assertEqual(compute_ess(np.array([-421.125251])), 1.0)
+        self.assertEqual(compute_ess(np.array([0.0])), 1.0)
 
 
 if __name__ == '__main__':
